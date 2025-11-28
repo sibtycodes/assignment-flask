78dfcec17a0341f8f86c6eae792e515198baf474
sibtycodes <a2zareallowed11@gmail.com>
2025-11-29 01:10:40 +0500

    all files ...

--- app.py
+++ app.py
@@ -86,4 +86,7 @@ def delete():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    host = os.environ.get('FLASK_RUN_HOST', '0.0.0.0')
+    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(host=host, port=port, debug=debug)
